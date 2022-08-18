b645941044c0fbdc296eb7a7af11cf422644635c
renshuo <renshuoo@qq.com>
2022-08-18 17:25:09 +0800

    更新版本发布配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/env/index.ts"),
+      name: 'cesium-env',
+    },
+    rollupOptions: {
+      external: [
+        'vue',
+        'cesium',
+      ],
+    }
+  }
 })
